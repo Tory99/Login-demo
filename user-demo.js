e63c333d6dc637cfea808220632a96b3a0a1137a
tory99 <hjw990826@gmail.com>
2025-02-19 16:42:09 +0900

    회원가입 완료

--- user-demo.js
+++ user-demo.js
@@ -37,19 +37,29 @@ app.post('/join', (req, res) => {
     }
 
     if(db.get(body.id) || db.get(body.name)){
-        res.send("중복된 아이디입니다.")
+        res.send("중복된 아이디나 이름입니다.")
     } else {
         let newuser = {
             pwd : body.pwd,
             name : body.name
         }
         db.set(body.id,newuser)
-        res.send()
+        res.send(`${db.get(body.id).name}님 회원가입에 되었습니다. \n 환영합니다.`)
     }
 })
 // 회원조회
 app.get('/users/:id', (req, res) => {
-    
+    let body = req.body
+
+    if(!body.id || !body.pwd){
+        return res.status(400).send("아이디와 비밀번호를 입력하세요")
+    }
+
+    if((db.get(body.id) == body.id ) || (db.get(body.id).pwd == body.pwd)){
+        res.json(`${db.get(body.id).name}님 환영합니다.`)
+    } else {
+        res.status(400).send("아이디나 비밀번호가 틀렸습니다.")
+    }
 })
 // 회원탈퇴
 app.delete('/users/:id', (req, res) => {
