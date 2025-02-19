e422a75863610bdf4d03e900961918a3d02cd67f
tory99 <hjw990826@gmail.com>
2025-02-19 18:11:25 +0900

    강의 완료료

--- user-demo copy.js
+++ user-demo copy.js
@@ -5,73 +5,76 @@ app.listen(3000)
 app.use(express.json())
 
 let db = new Map()
+let id = 2
 
 let exlogin = {
+    userid : "HJW",
     pwd : 12341234,
     name : "Jeongwoo"
 }
 
-db.set('HJW',exlogin)
+db.set(1,exlogin)
 
 // 로그인 
 
 app.post('/login', (req, res) => {
     let body = req.body
-    const user = db.get(body.id)
+    const user = db.get(id)
 
     if(!body.id || !body.pwd){
         return res.status(400).send("아이디와 비밀번호를 입력하세요")
     }
 
-    if((user == body.id ) || (user.pwd == body.pwd)){
+    if((user.id == body.id ) || (user.pwd == body.pwd)){
         res.json(`${user.name}님 환영합니다.`)
     } else {
         res.status(400).send("아이디나 비밀번호가 틀렸습니다.")
     }
 })
+
 // 회원가입
 app.post('/join', (req, res) => {
     let body = req.body
+    let user = db.get(id)
 
-    if(!body.id || !body.pwd || !body.name){
+    if(!body.userid || !body.pwd || !body.name){
         return res.status(400).send("아이디와 비밀번호와 이름을을 입력하세요")
     }
 
-    if(db.get(body.id) || db.get(body.name)){
+    if(db.get(body.userid) || db.get(body.name)){
         res.send("중복된 아이디나 이름입니다.")
     } else {
-        let newuser = {
-            pwd : body.pwd,
-            name : body.name
-        }
-        db.set(body.id,newuser)
-        res.send(`${db.get(body.id).name}님 회원가입에 되었습니다. \n 환영합니다.`)
+        db.set(id++,body)
+        res.send(`${db.get(id-1).name}님 회원가입에 되었습니다. \n 환영합니다.`)
     }
 })
 // 회원조회
 app.get('/users/:id', (req, res) => {
-    let id = req.params
+    let {id} = req.params
+    id = parseInt(id)
 
-    console.log(id)
     const user = db.get(id)
-    db.forEach((values ,key) =>{
-        console.log(key)
-        if(key == id){
-            console.log("goov")
-            res.json(`${user.name}님의 정보입니다.\n
-                                 id : ${id} \n 
-                                 pwd : ${db.get(id).pwd} \n 
-                                name : ${db.get(id).name}`)
-        }
-    })
-    // if(db.has(id)){
-    //     res.send(`${user.name}님의 정보입니다.\n
-    //                 id : ${id} \n 
-    //                 pwd : ${db.get(id).pwd} \n 
-    //                 name : ${db.get(id).name}`)
-    // }
+    if(user == undefined){
+        res.status(404).json("회원정보가 없습니다.")
+    } else {
+        res.status(200).json({
+            userid : user.userid,
+            name : user.name,
+        })
+    }
 })
 // 회원탈퇴
 app.delete('/users/:id', (req, res) => {
+    let {id} = req.params
+    id = parseInt(id)
+
+    const user = db.get(id)
+    if(user == undefined){
+        res.status(404).json("회원정보가 없습니다.")
+    } else {
+        let username = user.name
+        db.delete(id)
+        res.status(200).json(`${username}님 삭제가 되었습니다.`)
+    }
     
 })
--- user-demo.js
+++ user-demo.js
@@ -20,13 +20,13 @@ app.post('/login', (req, res) => {
     const user = db.get(body.id)
 
     if(!body.id || !body.pwd){
-        return res.status(400).send("아이디와 비밀번호를 입력하세요")
+        return res.status(404).send("아이디와 비밀번호를 입력하세요")
     }
 
     if((user == body.id ) || (user.pwd == body.pwd)){
         res.json(`${user.name}님 환영합니다.`)
     } else {
-        res.status(400).send("아이디나 비밀번호가 틀렸습니다.")
+        res.status(404).send("아이디나 비밀번호가 틀렸습니다.")
     }
 })
 // 회원가입
@@ -34,7 +34,7 @@ app.post('/join', (req, res) => {
     let body = req.body
 
     if(!body.id || !body.pwd || !body.name){
-        return res.status(400).send("아이디와 비밀번호와 이름을을 입력하세요")
+        return res.status(404).send("아이디와 비밀번호와 이름을을 입력하세요")
     }
 
     if(db.get(body.id) || db.get(body.name)){
@@ -50,28 +50,28 @@ app.post('/join', (req, res) => {
 })
 // 회원조회
 app.get('/users/:id', (req, res) => {
-    let id = req.params
+    let id = req.params.id
 
-    console.log(id)
     const user = db.get(id)
-    db.forEach((values ,key) =>{
-        console.log(key)
-        if(key == id){
-            console.log("goov")
-            res.json(`${user.name}님의 정보입니다.\n
-                                 id : ${id} \n 
-                                 pwd : ${db.get(id).pwd} \n 
-                                name : ${db.get(id).name}`)
-        }
-    })
-    // if(db.has(id)){
-    //     res.send(`${user.name}님의 정보입니다.\n
-    //                 id : ${id} \n 
-    //                 pwd : ${db.get(id).pwd} \n 
-    //                 name : ${db.get(id).name}`)
-    // }
+    if(db.has(id)){
+        res.send(`${user.name}님의 정보입니다.\n
+                    id : ${id} \n 
+                    pwd : ${db.get(id).pwd} \n 
+                    name : ${db.get(id).name}`)
+    } else {
+        res.status(404).send("입력하신 아이디는 없습니다.")
+    }
 })
 // 회원탈퇴
 app.delete('/users/:id', (req, res) => {
-    
+    let id = req.params.id
+
+    const user = db.get(id)
+    if(db.has(id)){
+        let username = user.name
+        db.delete(id)
+        res.send(`${username}님 탈퇴되었습니다.`)
+    } else {
+        res.status(404).send("입력하신 아이디는 없습니다.")
+    }
 })
